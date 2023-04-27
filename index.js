9daa06f3643ddcf6f08eef3e81fe1eebaf65a270
Gaya Haroun <gharoun94@gmail.com>
2023-04-27 01:24:15 -0500

    extract all routes in index.js to routes.js

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
-import genreRouter from "./routes/genre.routes.js";
-import customerRouter from "./routes/customer.routes.js";
-import movieRouter from "./routes/movie.routes.js";
-import rentalRouter from "./routes/rental.routes.js";
-import userRouter from "./routes/user.routes.js";
-import authRouter from "./routes/auth.routers.js";
-import error from "./middleware/error.js";
 import connectDB from "./mongodb/connect.js";
+import logger from "./middleware/loggingErrors.js";
 import express from "express";
 import * as dotenv from "dotenv";
-import logger from "./middleware/loggingErrors.js";
+import routes from "./startup/routes.js";
 
 dotenv.config();
 const app = express();
 
 process.on("uncaughtException", (ex) => {
-  console.log("WE GOT AN UNCaught Exception");
   logger.error(new Error(ex.message));
+  process.exit(1);
+});
+process.on("unhandledRejection", (ex) => {
+  logger.error(new Error(ex.message));
+  process.exit(1);
 });
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" });
-});
-app.use("/api/genres", genreRouter);
-app.use("/api/customers", customerRouter);
-app.use("/api/movies", movieRouter);
-app.use("/api/rentals", rentalRouter);
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use(error);
+routes(app);
 
 const port = process.env.PORT || 3000;
 const startServer = async () => {
---
+++ startup/routes.js
@@ -0,0 +1,22 @@
+import genreRouter from "../routes/genre.routes.js";
+import customerRouter from "../routes/customer.routes.js";
+import movieRouter from "../routes/movie.routes.js";
+import rentalRouter from "../routes/rental.routes.js";
+import userRouter from "../routes/user.routes.js";
+import authRouter from "../routes/auth.routers.js";
+import error from "../middleware/error.js";
+import express from "express";
+
+export default function (app) {
+  app.use(express.json());
+  app.get("/", (req, res) => {
+    res.send({ message: "Hello World!" });
+  });
+  app.use("/api/genres", genreRouter);
+  app.use("/api/customers", customerRouter);
+  app.use("/api/movies", movieRouter);
+  app.use("/api/rentals", rentalRouter);
+  app.use("/api/users", userRouter);
+  app.use("/api/auth", authRouter);
+  app.use(error);
+}
